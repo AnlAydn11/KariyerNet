193b71ac139b90df0a80fb67ae65f7878569d0a9
AnlAydn11 <120140629+AnlAydn11@users.noreply.github.com>
2025-01-30 15:55:07 +0300

    Update Profile.jsx

--- IsProjesiClient-main/src/components/Profile.jsx
+++ IsProjesiClient-main/src/components/Profile.jsx
@@ -10,7 +10,7 @@ export default function Profile() {
     const fetchApplications = async () => {
       try {
         const token = localStorage.getItem("token");
-        const response = await axios.get("http://localhost:5050/profileDetail/applications", {
+        const response = await axios.get("https://kariyernet-6.onrender.com/profileDetail/applications", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
